f4fae43023e72fdb16ed8374a24c5bf600a28944
RoyNyaga <nyagaandreroy@gmail.com>
2021-06-26 16:16:34 +0100

    setup channels and create rooms controller

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,15 +1,24 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener("turbolinks:load", () => {
+  const element = document.getElementById("room-id")
+  const room_id = element.getAttribute("data-room-id")
+  console.log(room_id)
+  consumer.subscriptions.create({ channel: "RoomChannel",
+  room_id: room_id}, {
+    connected() {
+      console.log("connected to room channel..." + room_id)
+      // Called when the subscription is ready for use on the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+    received(data) {
+      console.log(data)
+      // Called when there's incoming data on the websocket for this channel
+    }
+  }); 
+})
+
